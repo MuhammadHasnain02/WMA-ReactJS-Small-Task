483cc4a7f132a3ee74744450398ff92eaaa637a2
MuhammadHasnain02 <mrhasnain9009@gmail.com>
2026-01-11 19:08:36 +0500

    done

--- reactjs-small-task/src/App.jsx
+++ reactjs-small-task/src/App.jsx
@@ -65,6 +65,69 @@
 
 // ==============================================================
 
+// import { useState } from "react";
+// import "./App.css";
+// import Result from "./components/Result";
+
+// function App() {
+
+//   // Students Array Of Object
+//   const students = [
+//     {
+//       name: "Ali",
+//       marks: 30,
+//       status: false,
+//     },
+//     {
+//       name: "Ahmed",
+//       marks: 60,
+//       status: true,
+//     },
+//     {
+//       name: "Raza",
+//       marks: 40,
+//       status: false,
+//     },
+//     {
+//       name: "Zubair",
+//       marks: 80,
+//       status: true,
+//     },
+//   ];
+
+//   // ALL | PASS | FAIL -> MODES
+//   const [ mode, setMode ] = useState("ALL");
+
+//   // Filtered Students
+//   const filteredStudents = 
+//     mode === 'ALL' 
+//     ? students 
+//     : mode === 'PASS' 
+//     ? students.filter((s) => s.status === true) 
+//     : students.filter((s) => s.status === false)
+
+//   return (
+//     <div>
+
+//       {/* Buttons */}
+//       <button onClick={() => setMode("PASS")}>PASS</button>
+//       <button onClick={() => setMode("FAIL")}>FAIL</button>
+
+//       {/* All OR Filteres Students */}
+//       {
+//         filteredStudents.map((m, i) => {
+//           return <Result key={i} marks={m.marks} name={m.name} />;
+//         })
+//       }
+
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// ==============================================================
+
 import { useState } from "react";
 import "./App.css";
 import Result from "./components/Result";
